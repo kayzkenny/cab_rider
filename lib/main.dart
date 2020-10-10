93ab83feb968fc516adcf33c60c6ceccb2a4feca
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-10-10 19:42:45 +0100

    build: configure firebase options for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,29 @@
+import 'dart:io';
+
 import 'package:cab_rider/screens/main_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final FirebaseApp app = await Firebase.initializeApp(
+    name: 'db2',
+    options: Platform.isIOS || Platform.isMacOS
+        ? FirebaseOptions(
+            appId: '1:297855924061:ios:c6de2b69b03a5be8',
+            apiKey: 'AIzaSyD_shO5mfO9lhy2TVWhfo1VUmARKlG4suk',
+            projectId: 'flutter-firebase-plugins',
+            messagingSenderId: '297855924061',
+            databaseURL: 'https://flutterfire-cd2f7.firebaseio.com',
+          )
+        : FirebaseOptions(
+            appId: '1:268282644915:android:7d569cdfce9b528e9ee3c6',
+            apiKey: 'AIzaSyA2K6lj6tSMD62FGv5sViZn40VD2i6ZxZM',
+            messagingSenderId: '268282644915',
+            projectId: 'geetaxi-1e769',
+            databaseURL: 'https://geetaxi-1e769.firebaseio.com',
+          ),
+  );
   runApp(MyApp());
 }
 
--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -11,6 +12,19 @@ class _MainPageState extends State<MainPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Main Page'),
+      ),
+      body: Center(
+        child: MaterialButton(
+          onPressed: () {
+            DatabaseReference dbRef =
+                FirebaseDatabase.instance.reference().child('Test');
+            dbRef.set('IsConnected');
+          },
+          height: 50,
+          minWidth: 300,
+          color: Colors.green,
+          child: Text('Test Connection'),
+        ),
       ),
     );
   }
