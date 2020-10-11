91f919c31da1eec1ba7378e34c2f3b5300a84c9f
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-10-11 19:36:37 +0100

    build: create map form

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cab_rider/screens/brand_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -31,6 +32,134 @@ class _MainPageState extends State<MainPage> {
               _controller.complete(controller);
               mapController = controller;
             },
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 15.0,
+                    spreadRadius: 0.5,
+                    offset: Offset(
+                      0.7,
+                      0.7,
+                    ),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: 18,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 5),
+                    Text(
+                      'Nice to see you!',
+                      style: TextStyle(fontSize: 10.0),
+                    ),
+                    Text(
+                      'Where are you going?',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontFamily: 'Brand-Bold',
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(4),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 5.0,
+                            spreadRadius: 0.5,
+                            offset: Offset(
+                              0.7,
+                              0.7,
+                            ),
+                          )
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.search,
+                              color: Colors.blueAccent,
+                            ),
+                            SizedBox(width: 10),
+                            Text('Search Destination'),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.home_outlined,
+                          color: BrandColors.colorDimText,
+                        ),
+                        SizedBox(width: 12),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Add Home'),
+                            SizedBox(height: 4),
+                            Text(
+                              'Your residential address',
+                              style: TextStyle(
+                                fontSize: 11,
+                                color: BrandColors.colorDimText,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.work_outline,
+                          color: BrandColors.colorDimText,
+                        ),
+                        SizedBox(width: 12),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Add Work'),
+                            SizedBox(height: 4),
+                            Text(
+                              'Your office address',
+                              style: TextStyle(
+                                fontSize: 11,
+                                color: BrandColors.colorDimText,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
