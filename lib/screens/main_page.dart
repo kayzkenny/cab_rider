fb0898aee759dd31d2cb6867692635f07504e6d4
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-10-11 19:10:46 +0100

    build: show google map

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,5 +1,7 @@
-import 'package:firebase_database/firebase_database.dart';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MainPage extends StatefulWidget {
   static const String id = 'mainpage';
@@ -9,25 +11,28 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  Completer<GoogleMapController> _controller = Completer();
+  GoogleMapController mapController;
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Main Page'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: MaterialButton(
-          onPressed: () {
-            DatabaseReference dbRef =
-                FirebaseDatabase.instance.reference().child('Test');
-            dbRef.set('IsConnected');
-          },
-          height: 50,
-          minWidth: 300,
-          color: Colors.green,
-          child: Text('Test Connection'),
-        ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+              mapController = controller;
+            },
+          ),
+        ],
       ),
     );
   }
