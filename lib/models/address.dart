d1869f6569c89d35dfd74eab50aac91f4b6bca74
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-10-12 13:21:44 +0100

    build: create address model class

---
+++ lib/models/address.dart
@@ -0,0 +1,15 @@
+class Address {
+  final String placeId;
+  final String placeName;
+  final String placeFormattedAddress;
+  final double latitude;
+  final double longitude;
+
+  Address({
+    this.placeId,
+    this.placeName,
+    this.placeFormattedAddress,
+    this.latitude,
+    this.longitude,
+  });
+}
